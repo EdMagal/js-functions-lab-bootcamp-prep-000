6e7b4777b58d64d07bdd8f89feff1f79f7faac6e
Eduardo Magalhaes <ed83isme@gmail.com>
2018-02-19 19:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
-function happyHolidays() {
-  return "Happy holidays!";
+function happyHolidays () {
+return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+function happyHolidaysTo (Joe) {
+return `Happy holidays, ${Joe}!`
 }
 
 function happyHolidayTo(holiday, name){
